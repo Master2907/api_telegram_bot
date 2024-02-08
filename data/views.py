d6b2aa60a1b9a157fc68b5cbc85d154b452aaffd
Umar Khashimov <93665237+Master2907@users.noreply.github.com>
2024-02-08 20:44:22 +0500

    Delete data directory

--- data/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from rest_framework.generics import ListAPIView
-from .models import DataModel
-from .serializers import DataSerializer
-# Create your views here.
-
-class DataList(ListAPIView):
-    queryset = DataModel.objects.all()
-    serializer_class = DataSerializer
